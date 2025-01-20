29cda541820c8f208c90221534d7b90aaa067e18
Saezvil <115928408+saezvil@users.noreply.github.com>
2025-01-20 18:10:56 +0300

    Hide setup.py

--- setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup
-from Cython.Build import cythonize
-
-setup(
-    name="Milkifier",
-    ext_modules=cythonize("MilkUP.pyx", compiler_directives={'language_level': "3"}),
-    zip_safe=False,
-)
